31713aa2247a6976a937a723b40b4609a2eb945f
msvreddy02 <msvreddy02@gmail.com>
2026-09-18 12:22:43 +0530

    Add NHSBT project

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: false, 
-    minify: 'terser', 
-    terserOptions: {
-      compress: {
-        drop_console: true, 
-        drop_debugger: true,
-      },
-    },
-  },
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
